edd3601f9d8a095334ea815d7294e995ab54acf1
Salvador Subarroca <subarroca@gmail.com>
2017-03-22 00:04:59 +0100

    fix(tag-by-word): remove es6 function failing in iPad

--- src/app/feeling/tag-by-word/tag-by-word.component.ts
+++ src/app/feeling/tag-by-word/tag-by-word.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import * as _ from 'lodash';
+
 import { QuestionService } from '../../core/question/question.service';
 import { ScreenComponent } from '../../shared/screen/screen.component';
 import { FlowControlService } from '../../core/flow-control/flow-control.service';
@@ -36,7 +38,7 @@ export class TagByWordComponent extends ScreenComponent implements OnInit {
   }
 
   get isAllSelected() {
-    return (<any>Object)
+    return _
       .values(this.matches)
       .filter(match => match)
       .length === this.words.length;
@@ -72,7 +74,7 @@ export class TagByWordComponent extends ScreenComponent implements OnInit {
   }
 
   isAlreadyMatchedPhoto(photo: Photo) {
-    return (<any>Object)
+    return _
       .values(this.matches)
       .includes(photo.id);
   }
