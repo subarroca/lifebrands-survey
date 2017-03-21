a0f6dd3de2f362166ab2376c4015809d3160d6f0
Salvador Subarroca <subarroca@gmail.com>
2017-03-22 00:08:31 +0100

    feat(style): improve readability in ipad

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  idleSecs: 3,
+  idleSecs: 20,
   questionsUrl: 'assets/data/questions.json',
   photosUrl: 'assets/data/photos.json',
   photosPath: 'assets/photos',
-  i18nUrl: '/assets/i18n',
+  i18nUrl: 'assets/i18n',
   langs: ['ca', 'es', 'en', 'eu', 'fr', 'it'],
   mainLang: 'ca',
-  apiUrl: 'poll'
+  apiUrl: '../poll'
 };
